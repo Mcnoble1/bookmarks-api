29d9fab2a54b3227fefa3c36e593471282ac7eb7
mcnoble1 <idowufestustemiloluwa@gmail.com>
2025-05-09 14:00:40 +0100

    chore: setup base file structure :octocat:

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+    signup() {
+        return { msg: 'I am signing up' };
+    }
+
+    signin() {
+        return { msg: 'I am signing in' };
+    }
+}
